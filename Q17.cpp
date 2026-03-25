9ff60e98277acc25c94fd208771763e05b248a86
imahiarora <mahi25csu135@ncuindia.edu>
2026-03-25 13:54:25 +0530

    Add checks for Perfect and Armstrong numbers

--- Q17.cpp
+++ Q17.cpp
@@ -1,32 +1,49 @@
 #include <iostream>
+#include <cmath>   // for pow()
 using namespace std;
 
 int main()
 {
-    int num, sum = 0, rem, temp;
+    int num, sum = 0, rem, temp, digits = 0;
+
     cout << "Enter a number: ";
     cin >> num;
-    // Check Perfect Number
-    for(int i = 1; i < num; i++)
+
+    // ----- Perfect Number -----
+    sum = 0;
+    for(int i = 1; i <= num / 2; i++)
     {
         if(num % i == 0)
         {
             sum += i;
         }
     }
-    if(sum == num)
+
+    if(sum == num && num != 0)
         cout << "Perfect Number" << endl;
     else
         cout << "Not a Perfect Number" << endl;
-    // Check Armstrong Number
+
+    // ----- Armstrong Number -----
+    temp = num;
+
+    // Count digits
+    while(temp > 0)
+    {
+        digits++;
+        temp /= 10;
+    }
+
     temp = num;
     sum = 0;
+
     while(temp > 0)
     {
         rem = temp % 10;
-        sum += rem * rem * rem;
+        sum += pow(rem, digits);  // generalized power
         temp /= 10;
     }
+
     if(sum == num)
         cout << "Armstrong Number";
     else
